bef54e4480e55f2cfca180bf72d7d56463573228
sidneylopes <sidneylopesjr@gmail.com>
2021-01-31 20:24:56 -0300

    Test 1

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
-<?php
-phpinfo();
+<?php 
+
+$servername = "localhost";
+$username = "sidney";
+$password = "32913013";
+$database = "system";
+
+
+$con = new mysqli($servername, $username, $password, $database);
+
+if($con->connect_error){
+    die("Connection failed: ".$con->connect_error);
+}
+
+echo "Connected successfully. ";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>System</title>
+</head>
+<body>
+    
+</body>
+</html>
